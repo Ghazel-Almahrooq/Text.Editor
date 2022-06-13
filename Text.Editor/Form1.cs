7c5358655cf163bf51732718a32013aecf53c643
غزل المحروق <AD0801@iu.edu.jo>
2022-06-14 00:40:37 +0300

    new

--- Text.Editor/Form1.cs
+++ Text.Editor/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Text.Editor
 {
-    public partial class Form1 : Form
+    public partial class frmTextEditor : Form
     {
-        public Form1()
+        public frmTextEditor()
         {
             InitializeComponent();
         }
